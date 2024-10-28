40c695715c362556092879c6a51d23d914517eac
bap2pecs <bap2pecs@proton.me>
2024-10-28 14:50:32 +0800

    fix header logo section

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 
-import { Logo } from "../Logo/Logo";
 import { ThemeToggle } from "../ThemeToggle/ThemeToggle";
 
 interface HeaderProps {}
@@ -11,8 +10,6 @@ export const Header: React.FC<HeaderProps> = () => {
       <div className="bg-base-300 shadow-sm">
         <div className="container mx-auto flex w-full items-center justify-between gap-4 p-6 pb-4 md:pb-6">
           <div className="flex items-center gap-4">
-            <Logo />
-            <span className=" text-3xl font-thin">×</span>
             <Image
               src="/snapchain-logo.jpg"
               alt="Snapchain"
@@ -20,6 +17,7 @@ export const Header: React.FC<HeaderProps> = () => {
               height={40}
               className="rounded-full"
             />
+            Snapchain Tohma Finality Explorer
           </div>
           <div className="flex items-center gap-4">
             {/* This modal can be enabled in development to display the connected FG RPC address */}
