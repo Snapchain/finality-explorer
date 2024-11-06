2f1385273cbe190acd7da0a9375b1ba941bf7f0d
parketh <yeungparkhay@gmail.com>
2024-11-06 18:55:11 +0000

    format logo

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -10,14 +10,8 @@ export const Header: React.FC<HeaderProps> = () => {
       <div className="bg-base-300 shadow-sm">
         <div className="container mx-auto flex w-full items-center justify-between gap-4 p-6 pb-4 md:pb-6">
           <div className="flex items-center gap-4">
-            <Image
-              src="/snapchain-logo.jpg"
-              alt="Snapchain"
-              width={40}
-              height={40}
-              className="rounded-full"
-            />
-            Snapchain Tohma Finality Explorer
+            <Image src="/logo.png" alt="Tohma" width={40} height={40} />
+            <p className="text-xl font-bold">Tohma Finality Explorer</p>
           </div>
           <div className="flex items-center gap-4">
             {/* This modal can be enabled in development to display the connected FG RPC address */}
